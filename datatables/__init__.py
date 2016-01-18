47ddeadf7c7a125b4d2a258f04f8a5539e6e200d
Sean Lawrence <lawrensm@gmail.com>
2016-01-18 12:07:32 -0800

    double quote around tablename in sort by clause

--- datatables/__init__.py
+++ datatables/__init__.py
@@ -327,7 +327,7 @@ class DataTables:
                     else:
                         tablename = parent.__table__.name
 
-            sort_name = '%s.%s' % (tablename, sort_name)
+            sort_name = '"%s".%s' % (tablename, sort_name)
             self.query = self.query.order_by(
                 asc(text(sort_name)) if sort.dir == 'asc' else desc(
                     text(sort_name)))
